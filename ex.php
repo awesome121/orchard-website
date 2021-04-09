1fcb8042b8dfefebd9ac47cd2ab79e8c35d6c7cf
Changxing Gong <51814631+awesome121@users.noreply.github.com>
2021-04-09 18:05:34 +1200

    Add files via upload

---
+++ ex.php
@@ -0,0 +1,66 @@
+
+<html>
+<head>
+    <title>Mill Creek Orchard</title>
+</head>
+
+    <body>
+      <?php
+        echo "<label>Username</label>";
+      ?>
+
+
+
+      <form>
+        <label>Username</label>
+        <br>
+        <input type="text" placeholder="username" required>
+        <br>
+        <label>Email</label>
+        <br>
+        <input type="email" placeholder="email" required>
+        <br>
+        <label>Password</label>
+        <br>
+        <input type="password" placeholder="password" required>
+        <br >
+        <label>Gender</label>
+        <br>
+        <input type="radio" name="Gender"> <label>Male</label>
+        <input type="radio" name="Gender"> <label>Female</label>
+        <input type="checkbox"> <label> I accept the terms and conditions </label>
+        <br>
+        <label>Message</label>
+        <br>
+        <textarea placeholder="Write your message here"></textarea>
+        <br>
+        <input type="file">
+        <br>
+        <select>
+          <option>Redmi</option>
+          <option>iPhone</option>
+        </select>
+        <br><br><br>
+        <button type="submit">submit</button>
+        <button type="reset">reset</button>
+        <!-- <label></label>
+        <select></select>
+        <option></option>
+        <textarea></textarea> -->
+
+      </form>
+
+
+    <body>
+
+    <!-- <body>
+      <h1>hello</h1>
+      <h2>hello</h2>
+      <p><i><b>aaaa</b>asdf</i>sdfasdf</p>
+      <small> small text </small>
+      <hr>
+      <a href="https://www.google.com"> Click here to Google</a>
+      <a href="./about.html"> Go to about page </a>
+    </body> -->
+
+</html>
